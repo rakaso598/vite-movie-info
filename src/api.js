725f7d2c4e2872e434cf623ddd5e9b9395ff4bed
rakaso598 <nexonaddress98@gmail.com>
2025-02-26 11:45:09 +0900

    feat: Added Query Params

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
-export const getReviews = async (order = "createdAt") => {
-  const query = `order=${order}`;
-  const res = await fetch(`https://learn.codeit.kr/0000/film-reviews?query`);
+export const getReviews = async ({
+  order = "createdAt",
+  offset = 0,
+  limit = 6,
+}) => {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
+  const res = await fetch(`https://learn.codeit.kr/0000/film-reviews?${query}`);
   const body = await res.json();
   return body;
 };
